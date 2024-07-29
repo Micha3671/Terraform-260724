e95638a95d36198a5d3ce95fa4df93f85e324a30
Micha <michael.rigas@t-online.de>
2024-07-29 09:56:24 +0200

    update

--- terraform/4_DynamoDB/main.tf
+++ terraform/4_DynamoDB/main.tf
@@ -1,10 +1,10 @@
 resource "aws_dynamodb_table" "my_dynamodb" {
-  name     = "user"
-  hash_key = "UserId"
+  name           = "user"
+  hash_key       = "UserId"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 1
-  
+
   attribute {
     name = "UserId"
     type = "S"
